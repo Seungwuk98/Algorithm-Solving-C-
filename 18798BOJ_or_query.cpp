ae2690744e7d8f393c64e40a836b1a3f251dcb5c
Seungwuk98 <88603100+Seungwuk98@users.noreply.github.com>
2022-05-01 00:27:05 +0900

    learn0430

--- 18798BOJ_or_query.cpp
+++ 18798BOJ_or_query.cpp
@@ -1,11 +1,12 @@
 #include <bits/stdc++.h>
+#define fastio ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
 
 using namespace std;
 
 int n,k,m;
-int arr[252525];
-int on[1010101];
-int sz[1010101];
+int on[1<<19];
+int sz[1<<19];
+const int hf = 1<<18;
 
 
 void merge(int x){
@@ -13,48 +14,47 @@ void merge(int x){
     sz[x] = sz[x<<1] + sz[x<<1|1];
 }
 
-void build(int s=1, int e=n, int x=1){
-    if (s==e) {
-        on[x] = arr[s-1];
-        sz[x] = arr[s-1]==k;
-        return;
-    }
-    int mid = s + e >> 1;
-    build(s, mid, x<<1); build(mid+1, e, x<<1|1);
-    merge(x);
-}
 
-void update(int z, int i, int s=1, int e=n, int x=1){
-    if (on[x]&z == z) return;
+void update(int z, int l, int r, int s=0, int e=hf-1, int x=1){
+    if (e < l || r < s || (on[x]&z) == z) return;
+    if (l <= s && e <= r) {
+        on[x] |= z;
+    }
     if (s==e) {
-        on[x]|=z;
         sz[x] = on[x]==k;
         return;
-    }
+    } 
+
     int mid = s + e >> 1;
-    if (i <= mid) update(z, i, s, mid, x<<1);
-    else update(z, i, mid+1, e, x<<1|1);
+    update(z, l, r, s, mid, x<<1);
+    update(z, l, r, mid+1, e, x<<1|1);
     merge(x);
 }
 
-int query(int l, int r, int s=1, int e=n, int x=1){
-    if (e < l || r < s) return 0;
-    if (l <= s && e <= r) return sz[x];
-    int mid = s+e>>1;
-    return query(l, r, s, mid, x<<1) + query(l, r, mid+1, e, x<<1|1);
+int query(int l, int r){
+    int ret = 0;
+    for (l|=hf, r|=hf; l<=r; l>>=1, r>>=1){
+        if (l&1) ret += sz[l++];
+        if (~r&1) ret += sz[r--];
+    }
+    return ret;
 }
 
 int main(){
     cin >> n >> k;
-    for (int i=0; i<n; ++i) cin >> arr[i];
-    build();
+    for (int i=1; i<=n; ++i) {
+        cin >> on[i|hf];
+        sz[i|hf] = on[i|hf]==k; 
+    }
+    for (int i=hf-1; i>0; --i) merge(i);
+
     cin >> m;
+    int op, l, r, z;
     while (m--){
-        int op, l, r, z;
         cin >> op >> l >> r;
         if (op == 1){
             cin >> z;
-            for (int i=l; i<=r; ++i) update(z, i);
+            update(z, l, r);
         } else {
             cout << query(l, r) << '\n';
         }
